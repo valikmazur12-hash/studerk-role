6329999dad026ec79e96d59a56f734295369cc83
valikmazur12-hash <valikmazur12@gmail.com>
2026-06-06 21:12:05 +0300

    Security: оновлено порт пошти та підключено Render Environment

--- itproger/itproger/settings.py
+++ itproger/itproger/settings.py
@@ -114,10 +114,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ====================================================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 10
 EMAIL_HOST_USER = 'valikmazur12@gmail.com'
 
 # Безпечно зчитуємо секретний токен з Render (Environment), а якщо його там немає — використовуємо поточний як резервний
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'kfzrsjwninchjlze')
-EMAIL_TIMEOUT = 15
