fadf08db3c93baa0fbaf68045b82183d857d26b8
valikmazur12-hash <valikmazur12@gmail.com>
2026-06-06 20:53:49 +0300

    Fix EMAIL_TIMEOUT

--- itproger/itproger/settings.py
+++ itproger/itproger/settings.py
@@ -114,10 +114,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ====================================================================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'valikmazur12@gmail.com'
 
 # Безпечно зчитуємо секретний токен з Render (Environment), а якщо його там немає — використовуємо поточний як резервний
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'givckwxhgerpmwaq')
-EMAIL_TIMEOUT = 7
+EMAIL_TIMEOUT = 15
